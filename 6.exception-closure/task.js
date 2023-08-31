8ee9d9a90a9ffbe0ef00a30715f32fd758696d23
Anna Kuzmina <123387584+WebByAnna@users.noreply.github.com>
2023-08-31 19:52:44 +0300

    upd

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value){
+    const parsedValue = Number.parseFloat(value);
+    if (Number.isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+function validateCount(value){
+try {
+    const parsedValue = parseCount(value);
+    return parsedValue;
+} catch (error) {
+    return error;
+}
+}
+
+class Triangle{
+    constructor(a, b, c){
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        
+    }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+}
+get perimeter(){
+    return this.a + this.b + this.c;
+}
+get area() {
+    const s = this.perimeter / 2;
+    const areaSquared = s * (s - this.a) * (s - this.b) * (s - this.c);
+    return Math.sqrt(areaSquared).toFixed(3);
+  }
+}
+function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+      return {
+        get perimeter() {
+          return 'Ошибка! Треугольник не существует';
+        },
+        get area() {
+          return 'Ошибка! Треугольник не существует';
+        }
+      };
+    }
+  }
+  
